56b27621b31154d902295d87c22fc44820802651
Māris Samats <maris.samats@gmail.com>
2025-08-31 22:26:14 +0300

    Unnecassary check

--- src/main.tsx
+++ src/main.tsx
@@ -35,11 +35,9 @@ declare module "@tanstack/react-router" {
 
 // Render the app
 const rootElement = document.getElementById("root")!;
-if (!rootElement.innerHTML) {
-	const root = ReactDOM.createRoot(rootElement);
-	root.render(
-		<StrictMode>
-			<RouterProvider router={router} />
-		</StrictMode>,
-	);
-}
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>,
+);
