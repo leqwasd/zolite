2e15fd96e661c2d285b099e9d489da601404f3d0
Māris Samats <maris.samats@gmail.com>
2025-08-31 22:26:30 +0300

    Discarded old commented code

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,3 @@
-// import { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
-// import { App } from "./App.tsx";
-// import { AppContextProvider } from "./AppContext/AppContextProvider.tsx";
-
-// createRoot(document.getElementById("root")!).render(
-// 	<StrictMode>
-// 		<AppContextProvider>
-// 			<App />
-// 		</AppContextProvider>
-// 	</StrictMode>
-// );
-
 import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import {
